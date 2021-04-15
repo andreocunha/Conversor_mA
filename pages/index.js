cb3363251b241967bf5eff7015823bd1d0bd4971
andreocunha <andreoliveiracunha20@gmail.com>
2021-04-15 13:39:33 -0300

    Correção da formula

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,30 @@ export default function Home() {
   const [valF, setValF] = useState(0);
   const [valPV, setValPV] = useState(0);
   const [valmA, setValmA] = useState(0);
+  const [valPorc, setValPorc] = useState(0);
+  const [valAITRT, setValAITRT] = useState(0);
 
   function calculaPV(I, F, PV){
-    if(Number(I) === 0 && Number(F) === 0){
+    if((Number(I) === 0 && Number(F) === 0) || Number(F) < Number(I)){
       setValPV(0);
+      setValAITRT(0);
+      setValPorc(0);
       return;
     }
 
-    let result = (16*(Number(PV) - Number(I)) / (Number(F) - Number(I))) + 4;
-    setValmA(result.toFixed(3));
+    if(PV < I && PV < F){
+      setValAITRT(0);
+      setValPorc(0);
+      return;
+    }
+
+    let resultmA = (16*(Number(PV) - Number(I)) / (Number(F) - Number(I))) + 4;
+    let resultPorc = ((resultmA - 4)*100)/16;
+    let resultAITRT = ((resultmA - 4)*28479)/16;
+
+    setValPorc(resultPorc.toFixed(2));
+    setValAITRT(resultAITRT.toFixed(2));
+    setValmA(resultmA.toFixed(2));
   }
 
   return (
@@ -30,25 +45,18 @@ export default function Home() {
       </h1>   
 
       <main className={styles.main}>
-        <div className={styles.inputArea}>
-          <h3>FINAL</h3>
-          <h3> = </h3>
-          <input 
-            className={styles.inputBox}
-            value={valF}
-            onChange={({ target: { value: radius } }) => { 
-              setValF(radius); 
-              calculaPV(valI, radius, valPV)
-            }}
-          />
-        </div>
+        <h1 className={styles.titlePV}>Valor em percentual: {valPorc} %</h1>
+        <h1 className={styles.titlePV}>Valor em corrente: {valmA} mA</h1>
+        <h1 className={styles.titlePV}>Valor AITRT: {valAITRT}</h1>
 
+        <h2>Entre com a faixa da U.E.:</h2>
         <div className={styles.inputArea}>
           <h3>INICIAL</h3>
           <h3> = </h3>
           <input 
             className={styles.inputBox}
             value={valI}
+            type={'number'}
             onChange={({ target: { value: radius } }) => { 
               setValI(radius); 
               calculaPV(radius, valF, valPV)
@@ -57,11 +65,26 @@ export default function Home() {
         </div>
 
         <div className={styles.inputArea}>
+          <h3>FINAL</h3>
+          <h3> = </h3>
+          <input 
+            className={styles.inputBox}
+            value={valF}
+            type={'number'}
+            onChange={({ target: { value: radius } }) => { 
+              setValF(radius); 
+              calculaPV(valI, radius, valPV)
+            }}
+          />
+        </div>
+
+
+        <div className={styles.inputArea}>
           <h3>PV</h3>
           <h3> = </h3>
           <input 
             type="range"
-            step={1}
+            step={0.01}
             min={valI} 
             max={valF}
             value={valPV}
@@ -73,14 +96,13 @@ export default function Home() {
           <input 
             className={styles.inputBox}
             value={valPV}
+            type={'number'}
             onChange={({ target: { value: radius } }) => { 
               setValPV(radius); 
               calculaPV(valI, valF, radius)
             }}
           />
         </div>
-
-        <h1 className={styles.titlePV}>{valmA} mA</h1>
 
       </main>
 
